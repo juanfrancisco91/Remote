00e3fb37ba74b8f25661338ebfb8f37d408bfa54
juanfranciso9110@gmail.com <juanfranciso9110@gmail.com>
2026-04-13 18:47:00 -0400

    Finalice el proyecto de webscrapping para el portafolio, lo pasare a una carpeta aparte para asi tenerlo en un espacio limpio

--- WebScraping/01.py
+++ WebScraping/01.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from conexion import conexion
+import os
 
 listas_urls = []
-entrada = '9110'
+entrada = '917110'
 i = 0
 # Bucle para recibir todas las urls, si es 0 se acabo
 while entrada != '0':
-    entrada = input(f'Ingrese la Url: ')
+    entrada = input(f'Ingrese la Url (Presiona el "0" Finalizar): ')
     # Condicional para evaluar si la entrada empieza con el protocolo https o http.
     if not entrada.startswith(('http:', 'https:')):
         print('No es una url (Necesita https o http)')
-        continue
     else:
         if '0' in entrada:
             continue
@@ -26,27 +26,63 @@ while entrada != '0':
                'https://es.wikipedia.org/wiki/Jorge_Montenegro_(escritor)']'''
 # IGNORAR URLS DE PRUEBA -------------------------------------------------------------
 
-listas_personas = []
-for i in listas_urls:
-    dict_values = {}
+# CONDICIONAL PARA VERIFICAR QUE SISTEMA OPERATIVO POSEE EL USUARIO Y ASI PODER COLOCARLE SU USER-AGENT RESPECTIVO
+system_os = os.uname().sysname
+if system_os == 'Linux':
+    agente = 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0'
+elif system_os == 'Mac':
+    agente = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+elif system_os == 'Windows':
+    agente = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0.'
 
-    soup = conexion(i, 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0')
-    dict_values['Titulo'] = soup.title.string
-    for z in soup.tbody.find_all('tr'):
-        th = z.find('th')
-        td = z.find('td')
-        if th is not None and td is not None:
-            if 'Nacimiento' in th.text:
-                dict_values['Nacimiento'] = td.text.strip()
+lista_dict = [] # LISTA PARA GUARDAR EL DICCIONARIO
+# BUCLE PARA ITERAR SOBRE LA LISTAS DE URLS
+if listas_urls:
+    for i in listas_urls:
 
-            elif 'Fallecimiento' in th.text:
-                dict_values['Fallecimiento'] = td.text.strip()
+        dict_values = {}
 
-            elif 'Causa de muerte' in th.text:
-                dict_values['Causa de muerte'] = td.text.strip()
-        else:
-            print('No contiene el atributo td o th')
-    listas_personas.append(dict_values)
-df = pd.DataFrame(listas_personas)
-df.to_csv('_urls.csv', index=False)
-print(f'{10*'*'} ARCHIVO CREADO {10*'*'}')
+        # LLAMADA A LA LIBRERIA CONEXION QUE CREAMOS Y A LA FUNCION conexion
+        # PARA PASARLE POR PARAMETROS CADA URL Y EL REPECTIVO AGENTE A UTILIZAR
+        soup = conexion(i, agente)
+
+        # LE PONEMOS EL TITULO A CADA PAGINA QUE LLEVE
+        dict_values['Titulo'] = soup.title.string
+
+        # BUSCAMOS DENTRO DEL TBODY QUE ES DONDE SE ALMACENAN LOS DATOS QUE NOS INTERESAN
+        # EN ESTE CASO LOS tr QUE A SU VEZ LLEVAN DENTRO LOS th y td QUE SON LOS QUE GUARDAN LA INFORMACION
+        # th SIENDO LA QUE CONTIENE  'titulos' Y td EL CUERPO QUE ESTAMOS BUSCADO
+        # th = Nacimiento
+        # th = 18 de Julio
+        for z in soup.tbody.find_all('tr'):
+            th = z.find('th')
+            td = z.find('td')
+
+            # DECLARAMOS UNA BUSQUEDA SEPARADA PARA SABER SI ESTAS APARECEN AMBAS EN LA ETIQUETA
+            # DE NO SER ASI INDICAMOS QUE NO CONTIENE ATRIBUTOS Y PASAMOS AL SIGUIENTE tr
+
+            # SI LOS CONTIENEN PASAMOS A UN CONDICIONAL QUE NOS APARTA LOS DATOS QUE QUEREMOS ENCONTRAR,
+            # NACIMIENTO, FALLECIMIENTO, CAUSA DE MUERTE
+            if th is not None and td is not None:
+                if 'Nacimiento' in th.text:
+                    dict_values['Nacimiento'] = td.text.strip()
+
+                elif 'Fallecimiento' in th.text:
+                    dict_values['Fallecimiento'] = td.text.strip()
+
+                elif 'Causa de muerte' in th.text:
+                    dict_values['Causa de muerte'] = td.text.strip()
+            else:
+                print('No contiene el atributo td y th')
+        # AGREGAMOS EL DICCIONARIO A UNA LISTA
+        lista_dict.append(dict_values)
+
+    # CONVERTIMOS A UN DATA FRAME CON PANDAS
+    df = pd.DataFrame(lista_dict)
+
+    # EXPORTAMOS EL ARCHIVO PARA SU MANIPULACION
+    df.to_csv('_urls.csv', index=False)
+    print(f'{10*'*'} ARCHIVO CREADO {10*'*'}')
+
+else:
+    print(f'{10*'x'} LISTA DE URLS VACIAS {10*'x'}')
--- WebScraping/conexion.py
+++ WebScraping/conexion.py
@@ -1,12 +1,16 @@
+# IMPORTACION DE LIBRERIAS PARA HACER LA PETICION A LA PAGINA
 import requests
 from bs4 import BeautifulSoup
 
+# CREACION DE FUNCION PARA ESTABLECER CONEXION, RECIBIENDO LA URL DEL SITIO QUE DEBE SER DE WIKIPEDIA
+# Y RECIBIENDO EL USER-AGENT QUE SE UTILIZARA
 def conexion(url,user_agent):
     url = url
     cabecera = {
         "User-Agent": user_agent}
     path = requests.get(url, headers=cabecera)
 
+    # CONDICIONAL PARA SABER CUANDO FALLE LA CONEXION
     if path.status_code == 200:
         soup = BeautifulSoup(path.text, 'html.parser')
         print(path.status_code)
